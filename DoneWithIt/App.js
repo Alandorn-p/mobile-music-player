08e7f52612561fd96b4434160d9c3d62a27f64df
Al Palanuwech <ap689@cornell.edu>
2022-08-03 16:01:19 +0700

    "third commit"

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+  Alert,
+  Button,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
+  let x = 1;
+  console.log("app started");
+  const pressStuff = () => console.log("pressed");
+  const alertStuff = () => null;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{ backgroundColor: "dodgerblue", width: "50%", height: 80 }}
+      ></View>
+    </SafeAreaView>
   );
 }
+
+const containerStyle = { backgroundColor: "orange" };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
