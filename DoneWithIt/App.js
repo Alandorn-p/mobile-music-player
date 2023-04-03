34c337aa5aaeb63af3eeb538386353e517293037
Al Palanuwech <ap689@cornell.edu>
2023-04-03 00:59:48 -0400

    removed app2

---
+++ DoneWithIt/App.js
@@ -0,0 +1,40 @@
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigation from "./app/navigation/AppNavigation";
+import AudioProvider from "./app/context/AudioProvider";
+import AudioListItem from "./app/components/AudioListItem";
+import TestText from "./app/components/TestText";
+
+import { View, Text } from "react-native";
+
+import NotificationBanner from "./app/context/NotificationBanner";
+
+import TestDownload from "./app/components/TestDownload";
+import filenamify from "react-native-filenamify";
+
+export default function App() {
+  try {
+    return <TestText></TestText>;
+  } catch (e) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ backgroundColor: "red" }}>{e.message}</Text>
+      </View>
+    );
+  }
+  // return (
+
+  // <AudioProvider>
+  //   <NotificationBanner>
+  //     <NavigationContainer>
+  //       <AppNavigation />
+  //     </NavigationContainer>
+  //   </NotificationBanner>
+  // </AudioProvider>
+  // );
+}
